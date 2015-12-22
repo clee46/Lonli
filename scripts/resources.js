73489cc00995bf89a63eb8e2f2391bf39b2a93e3
natepecota <nate.pecota@gmail.com>
2015-12-22 13:46:56 -0800

    Add resources page with functionality.

---
+++ scripts/resources.js
@@ -0,0 +1,31 @@
+$(function() {
+  $('.box').hide();
+  $('select').change(function() {
+    $(this).find('option:selected').each(function() {
+      if($(this).attr('value')=='depression') {
+        $('.box').not('.depression').hide();
+        $('.depression').show();
+      }
+      else if($(this).attr('value')=='anxiety') {
+        $('.box').not('.anxiety').hide();
+        $('.anxiety').show();
+      }
+      else if($(this).attr('value')=='therapy') {
+        $('.box').not('.therapy').hide();
+        $('.therapy').show();
+      }
+      else if($(this).attr('value')=='medication') {
+        $('.box').not('.medication').hide();
+        $('.medication').show();
+      }
+      else if($(this).attr('value')=='selfharm') {
+        $('.box').not('.selfharm').hide();
+        $('.selfharm').show();
+      }
+      else if($(this).attr('value')=='community') {
+        $('.box').not('.community').hide();
+        $('.community').show();
+      }
+    });
+  });
+});
