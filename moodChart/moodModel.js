7b7f1b57fb6f49682747cdea380e74659ee5315f
Robert Hill <codingbyrobert@gmail.com>
2015-12-22 10:39:18 -0800

    chart loading from local storage

--- moodChart/moodModel.js
+++ moodChart/moodModel.js
@@ -3,12 +3,12 @@ var moodDataArray = [];
 
 moodData.newDate = new Date().toISOString().slice(0,10);
 moodData.rating = null;
-moodData.exercise = false;
+moodData.exercise = null;
 moodData.sleep = null;
 moodData.weather = null;
-moodData.eat = false;
-moodData.meds = false;
-moodData.drugs = false;
+moodData.eat = null;
+moodData.meds = null;
+moodData.drugs = null;
 
 moodData.saveChart = function(chart) {
   var savedChart = JSON.stringify(chart);
@@ -18,6 +18,12 @@ moodData.saveChart = function(chart) {
 moodData.loadData = function() {
   var storedData = localStorage.getItem('moodChartData');
   var parsedData = JSON.parse(storedData);
+  if(parsedData != null){
+    parsedData.forEach(function(item) {
+      // moodView.tempDateArray.push(val);
+      moodDataArray.push(item);
+    });
+  }
   console.log(parsedData);
 };
 
@@ -27,15 +33,21 @@ moodData.getData = function() {
     moodData.rating = $('input[name="mood"]:checked').val();
     moodData.exercise = $('input[name="exercise"]:checked').val();
     moodData.sleep = $('input[name="sleep"]:checked').val();
-    moodData.weather = $('#weather option:selected').val();
+    moodData.weather = $('input[name="weather"]:checked').val();
     moodData.eat = $('input[name="eat"]:checked').val();
     moodData.meds = $('input[name="meds"]:checked').val();
     moodData.drugs = $('input[name="drugs"]:checked').val();
+//Why won't you SEE THIS
+    if(!moodData.rating || !moodData.exercise || !moodData.sleep || !moodData.weather || !moodData.eat || !moodData.meds || !moodData.drugs){
+      alert('Please check all options');
+      return;
+    }
     console.log(moodData);
     moodDataArray.push(moodData);
     moodData.saveChart(moodDataArray);
     moodView.makeChart();
   });
 };
-
+// Controller
+moodData.loadData();
 moodData.getData();
--- moodChart/moodView.js
+++ moodChart/moodView.js
@@ -1,6 +1,12 @@
 var moodView = {};
 moodView.tempMoodArray = [];
 moodView.tempDateArray = [];
+moodView.tempSleepArray = [];
+moodView.tempWeatherArray = [];
+moodView.tempExerciseArray = [];
+moodView.tempEatArray = [];
+moodView.tempMedsArray = [];
+moodView.tempDrugsArray = [];
 
 moodView.makeChart = function() {
   //Needed to stop chart flickering
@@ -10,6 +16,30 @@ moodView.makeChart = function() {
   moodDataArray.forEach(function(item) {
     var val = item.rating;
     moodView.tempMoodArray.push(val);
+  });
+  moodDataArray.forEach(function(item) {
+    var val = item.exercise;
+    moodView.tempExerciseArray.push(val);
+  });
+  moodDataArray.forEach(function(item) {
+    var val = item.sleep;
+    moodView.tempSleepArray.push(val);
+  });
+  moodDataArray.forEach(function(item) {
+    var val = item.weather;
+    moodView.tempWeatherArray.push(val);
+  });
+  moodDataArray.forEach(function(item) {
+    var val = item.eat;
+    moodView.tempEatArray.push(val);
+  });
+  moodDataArray.forEach(function(item) {
+    var val = item.meds;
+    moodView.tempMedsArray.push(val);
+  });
+  moodDataArray.forEach(function(item) {
+    var val = item.drugs;
+    moodView.tempDrugsArray.push(val);
   });
 
 //Here to grab date
@@ -30,6 +60,66 @@ moodView.makeChart = function() {
         pointHightlightFill: '#fff',
         pointHighlightStroke: 'rgba(220,220,220,1)',
         data: moodView.tempMoodArray
+      },
+      {
+        label: 'Exercise',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHightlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: moodView.tempExerciseArray
+      },
+      {
+        label: 'Sleep',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHightlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: moodView.tempSleepArray
+      },
+      {
+        label: 'Weather',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHightlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: moodView.tempWeatherArray
+      },
+      {
+        label: 'Ate well',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHightlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: moodView.tempEatArray
+      },
+      {
+        label: 'Taken meds',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHightlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: moodView.tempMedsArray
+      },
+      {
+        label: 'Taken Drugs',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHightlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: moodView.tempDrugsArray
       }
     ]
   };
@@ -38,6 +128,10 @@ moodView.makeChart = function() {
   moodView.lineChart = new Chart(moodView.ctx).Line(moodView.data);
   //Options here
   Chart.defaults.global.responsive = true;
+  Chart.defaults.global.multiTooltipTemplate = '<%= datasetLabel %> - <%= value %>';
 };
-
-moodView.makeChart();
+// Controller
+$(function() {
+  moodView.makeChart();
+  
+});
