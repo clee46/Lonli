cbeb93475da42d20e2394db19f1e158a20a8fcea
Robert Hill <codingbyrobert@gmail.com>
2015-12-23 15:50:54 -0800

    refactor done

--- scripts/resourcesView.js
+++ scripts/resourcesView.js
@@ -1,36 +1,13 @@
 var resourcesView = {};
-
 resourcesView.filterHandler = function() {
-  // $('.box').hide();
-  $('select').change(function() {
-    $(this).find('option:selected').each(function() {
-      if($(this).attr('value')=='depression') {
-        $('.box').not('.depression').hide();
-        $('.depression').show();
-      }
-      else if($(this).attr('value')=='anxiety') {
-        $('.box').not('.anxiety').hide();
-        $('.anxiety').show();
-      }
-      else if($(this).attr('value')=='therapy') {
-        $('.box').not('.therapy').hide();
-        $('.therapy').show();
-      }
-      else if($(this).attr('value')=='medication') {
-        $('.box').not('.medication').hide();
-        $('.medication').show();
-      }
-      else if($(this).attr('value')=='selfharm') {
-        $('.box').not('.selfharm').hide();
-        $('.selfharm').show();
-      }
-      else if($(this).attr('value')=='community') {
-        $('.box').not('.community').hide();
-        $('.community').show();
-      }
-      else if($(this).attr('value')=='all') {
-        $('.box').show();
-      }
-    });
+  $('#resources').on('change', function(event) {
+    $('.box').show();
+    console.log($('#resources').val());
+    if ($('#resources').val() === 'all') {
+      $('#resources-tab').show();
+    }
+    else {
+      $('.box:not('+'.' + this.value + ')').hide();
+    }
   });
 };
