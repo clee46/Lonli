24a4a244c789f7e31ddb27a17f27231a239efec6
Robert Hill <codingbyrobert@gmail.com>
2015-12-24 17:56:32 -0800

    should be working right now

--- scripts/util.js
+++ scripts/util.js
@@ -1,5 +1,5 @@
 util = {};
 //I don't think this will stop all XSS attacks, but some.
 util.sanitize = function(text){
-  text.replace(/<script[^>]*>/gi, ' <!-- ');
+  return text.replace(/<script[^>]*>/gi, ' <!-- ');
 };
